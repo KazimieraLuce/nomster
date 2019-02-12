731827aa79a5607e4c4c9b6f8a40ecad336801bb
Kazimiera Luce <kazimiera.luce@aya.yale.edu>
2019-02-12 22:09:39 +0000

    configuring Notification mailer for comment added

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "kazialuce@gmail.com",
+    mail(to: "kazia.m.luce@gmail.com",
       subject: "A comment has been added to your place")
   end
 end
