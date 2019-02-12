ca4dfae1a98f6f8b5924b2e68c5526a9809d52b3
Kazimiera Luce <kazimiera.luce@aya.yale.edu>
2019-02-12 02:41:23 +0000

    updating photo uploader

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,6 @@
-class Photo < ApplicationRecord
+class Photo < ApplicationRecord 
   belongs_to :user
   belongs_to :place
-end
-
-class Picture <ActiveRecord::Base
   mount_uploader :picture, PictureUploader
-end 
+end
+ 
